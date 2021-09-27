7bf188f51553c408f1a708dd8daff0db22473ad9
ValletteY <vallette.y@gmail.com>
2021-09-28 00:22:00 +0200

    v2021

--- models/dbconfig.js
+++ models/dbconfig.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-    "mongodb+srv://yvallette:Azqswx123+@cluster0.rblth.mongodb.net/test",
+    "mongodb+srv://ID:PASSword@cluster0.rblth.mongodb.net/test",
     { useNewURLParser: true, useUnifiedTopology: true},
     (err) => {
         if (!err) 
